184613238fb86990f84dd26ab187d3759de0ceeb
kmaheshbabu75 <maheshbabu75k7@gmail.com>
2026-06-26 01:54:50 +0530

    modified commit

---
+++ src/Loops/factorial.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class factorial {
+    static void main(String[] args) {
+        int num = 7;
+        int factorial = 1;
+
+        for(int i  = 1; i <= num; i++ ){
+            factorial *= i ;
+        }
+        System.out.println(factorial);
+    }
+}
